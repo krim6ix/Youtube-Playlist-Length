9837fcd186dbce1f807ceab201ce0234f183f69c
khatri-tushar <tushar.khatri.it19@nsut.ac.in>
2021-03-16 12:49:22 +0530

    fixed bug, Now works for all playlist lengths

---
+++ c
@@ -0,0 +1,29 @@
+[33mcommit 0b8bcbe81819fa89ccc1e8006fa976bb097a17a0[m[33m ([m[1;36mHEAD -> [m[1;32mmain[m[33m)[m
+Author: khatri-tushar <tushar.khatri.it19@nsut.ac.in>
+Date:   Tue Mar 16 02:09:03 2021 +0530
+
+    works for playlist size < 50. Will fix
+
+[33mcommit 4fbd4cf7d099be0137b429d8b1f40890233b6ebd[m
+Author: khatri-tushar <tushar.khatri.it19@nsut.ac.in>
+Date:   Tue Mar 16 01:35:39 2021 +0530
+
+    added gitignore
+
+[33mcommit ecec8386e21428ea8640f993984b3a61a8db3efe[m
+Author: khatri-tushar <tushar.khatri.it19@nsut.ac.in>
+Date:   Tue Mar 16 01:34:53 2021 +0530
+
+    changed gitignore
+
+[33mcommit dfe1e8a773a7ebdbb832c7a99825550f3f9e3d6e[m
+Author: khatri-tushar <tushar.khatri.it19@nsut.ac.in>
+Date:   Tue Mar 16 01:34:00 2021 +0530
+
+    added youtube api
+
+[33mcommit 075be3b8e41dd1b10856ca7fd2618d16732b75f4[m
+Author: khatri-tushar <tushar.khatri.it19@nsut.ac.in>
+Date:   Tue Mar 16 01:31:17 2021 +0530
+
+    api connected
--- public/script.js
+++ public/script.js
@@ -1,41 +1,23 @@
 let LENGTH=0;
 let i = ytLink.search('playlist?');
 let playlistID = ytLink.slice(i+14)
-
-//duration is of the form PT11M22S i.e 11min22sec
-function extractLength(str){
-    console.log("this is extract length ")
-    let m = str.search('M');
-    let s = str.search('S')
-    let min = parseInt(str.slice(2,m));
-    let sec = parseInt(str.slice(m+1,s));
-    return (min*60)+sec;
-}
-
-function calculateLength(data){
-    console.log("this is calculate length ")
-    for(let i of data.items){
-        let item = i.contentDetails.duration;
-        LENGTH += extractLength(item)
-    }
-}
-function fetchVideos(videoID){
-    console.log("this is fetch videos ")
-    var xhrRequest = new XMLHttpRequest();
-    xhrRequest.onload = function(){
-        var response = JSON.parse(xhrRequest.response);
-        calculateLength(response);
-    }
-    xhrRequest.open('get',`https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails&id=${videoID}&key=AIzaSyCPOzYkvB9zqXIbfzwpwuIctgvbxqTeviE`,true)
-    xhrRequest.send();
-}
+let linkList = [];
 
 function iterateVideos(data){
-    
     for(let i of data.items){
         let item = i.snippet.resourceId.videoId;
-        // $('ul').append(`<li>${item}</li>`)
-        fetchVideos(item);
+        linkList.push(item);
+    }
+
+    //if data.nextPageToken is defined then there are more videos on the next page so fetch that page data also 
+    if(data.nextPageToken){
+        var xhrRequest = new XMLHttpRequest();
+        xhrRequest.onload = function(){
+            var response = JSON.parse(xhrRequest.response);
+            iterateVideos(response);
+        }
+        xhrRequest.open('get',`https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=1000&pageToken=${data.nextPageToken}&playlistId=${playlistID}&key=AIzaSyCMEUtFkCZtqJNNTFylBthfQs6WpVl8L-Y`,true)
+        xhrRequest.send();
     }
 }
 
@@ -45,21 +27,79 @@ function fetchYtplaylist(){
         var response = JSON.parse(xhrRequest.response);
         iterateVideos(response);
     }
-    xhrRequest.open('get',`https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=1000&playlistId=${playlistID}&key=AIzaSyCPOzYkvB9zqXIbfzwpwuIctgvbxqTeviE`,true)
+    xhrRequest.open('get',`https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=1000&playlistId=${playlistID}&key=AIzaSyCMEUtFkCZtqJNNTFylBthfQs6WpVl8L-Y`,true)
     xhrRequest.send();
 }
 
 fetchYtplaylist();
+
+// duration is of the form PT11M22S i.e 11min22sec
+parseISO8601Duration = function (iso8601Duration) {
+    var iso8601DurationRegex = /(-)?P(?:([.,\d]+)Y)?(?:([.,\d]+)M)?(?:([.,\d]+)W)?(?:([.,\d]+)D)?T(?:([.,\d]+)H)?(?:([.,\d]+)M)?(?:([.,\d]+)S)?/;
+
+    var matches = iso8601Duration.match(iso8601DurationRegex);
+    var days = parseInt(matches[5] === undefined ? 0 : matches[5]);
+    var hours = parseInt(matches[6] === undefined ? 0 : matches[6]);
+    var minutes = parseInt(matches[7] === undefined ? 0 : matches[7]);
+    var seconds = parseInt(matches[8] === undefined ? 0 : matches[8]);
+    return days*86400 + hours * 3600 + minutes * 60 + seconds;
+}
+    
+
+
+function calculateLength(data){
+    for(let i of data.items){
+        let item = i.contentDetails.duration;
+        var temp=0;
+        temp += parseISO8601Duration(item)
+        LENGTH+=temp;
+        //debugging
+        // var hours = Math.floor(LENGTH / 60 / 60);
+        // var minutes = Math.floor(LENGTH / 60) - (hours * 60);
+        // var seconds = LENGTH % 60;
+        // var hours1 = Math.floor(temp / 60 / 60);
+        // var minutes1 = Math.floor(temp / 60) - (hours1 * 60);
+        // var seconds1 = temp % 60;
+        // $('.main-body').append(`<p>Length of the Playlist is ${hours} Hours, ${minutes} minutes, ${seconds} seconds....... ${hours1} Hours, ${minutes1} minutes, ${seconds1} seconds</p>`)
+
+    }
+}
+function fetchVideos(videoID){
+    var xhrRequest = new XMLHttpRequest();
+    xhrRequest.onload = function(){
+        var response = JSON.parse(xhrRequest.response);
+        calculateLength(response);
+    }
+    xhrRequest.open('get',`https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails&id=${videoID}&key=AIzaSyCMEUtFkCZtqJNNTFylBthfQs6WpVl8L-Y`,true)
+    xhrRequest.send();
+}
+
+setTimeout(() => {
+    console.log(linkList)
+    for(let i of linkList){
+        fetchVideos(i);
+    }
+}, 1500);
+
 
 setTimeout(function(){
     var hours = Math.floor(LENGTH / 60 / 60);
     var minutes = Math.floor(LENGTH / 60) - (hours * 60);
     var seconds = LENGTH % 60;
     $('.main-body').append(`<p>Length of the Playlist is ${hours} Hours, ${minutes} minutes, ${seconds} seconds</p>`)    
-},1000)
+},3000)
 
-// $.ajax({
-//     url:'https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=1000&playlistId=PLdnsORcN_8yi1iEPXagU0acwtChNQShFe&key=AIzaSyCPOzYkvB9zqXIbfzwpwuIctgvbxqTeviE',
-//     method:'GET',
-//     success:displayName
-// })
+
+// setTimeout(function(){
+//     console.log(linkList)
+// },1000)
+
+// 'https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&pageToken=CGQQAQ&playlistId=PL64Td87Z2s0fC0GqdmN4xiGP_kkum9y-R&key=[YOUR_API_KEY]' \
+
+
+
+
+//api keys
+//AIzaSyBXA4xoznOYRpzu4QJ5UJoftPPnSdMfsro
+//AIzaSyCPOzYkvB9zqXIbfzwpwuIctgvbxqTeviE
+//AIzaSyCMEUtFkCZtqJNNTFylBthfQs6WpVl8L-Y
